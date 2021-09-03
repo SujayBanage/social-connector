b7804bc7b9769a6977a87bcad716859544618bd4
sujay <sujaybanage16032001@gmail.com>
2021-09-03 15:13:21 +0530

    final commit

--- client/src/app.js
+++ client/src/app.js
@@ -5,7 +5,6 @@ import Login from "./components/login/login.js";
 import Signup from "./components/signup/signup.js";
 import Feed from "./components/feed/feed.js";
 import Postupload from "./components/postupload/postupload.js";
-// import Fileupload from "./components/fileupload/fileupload.js";
 import Profileupload from "./components/profileupload/profileupload.js";
 import Notifications from "./components/Notifications/notifications.js";
 import Friends from "./components/Friends/Friends.js";
@@ -18,48 +17,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import './app.css';
 
-// import apiRequestService from "./services/apiRequestService.js";
-// const apiRequest = new apiRequestService();
-// let user;
-// const setValue = (data, val) => {
-//   data = val;
-// };
-
-// async function fetchData() {
-//   const res = await apiRequest.fetchPrivateData(
-//     "/private/getuserdata",
-//     sessionStorage.getItem("authToken")
-//   );
-//   return res.data.user;
-// }
-// fetchData()
-//   .then((data) => {
-//     setValue(user, data);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-// console.log(user);
-
-
 const App = () => {
-  // const [user,setUser] = useState({});
-
-  // useEffect(() => {
-
-  //     async function fetchData() {
-  //         const res = await apiRequest.fetchPrivateData('/private/getuserdata',sessionStorage.getItem("authToken"));
-  //         console.log(res);
-  //         setUser(res.data.user);
-  //    }
-
-  //    fetchData();
-
-  // },[])
-
-  // const userContext = createContext(user);
-
   return (
     <>
       <Router>
