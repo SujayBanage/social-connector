9502e55ed880fd01ad2daae6b598acd01eba5d35
sujay <sujaybanage16032001@gmail.com>
2021-10-09 14:37:50 +0530

    final commit

--- client/src/UIshared/card/card.js
+++ client/src/UIshared/card/card.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext} from "react";
 import "./card.css";
 import { Link, useHistory } from "react-router-dom";
 import like from "../../images/like.png";
